c2561ea476211ba694843f6a5ecc561b9d823244
Ringo Van den Hende <ringo.vandenhende@faros.be>
2024-04-10 09:11:54 +0200

    add exceptionhandler

---
+++ src/main/java/be/faros/sandwichbar/exception/SandwichbarErrorResponse.java
@@ -0,0 +1,29 @@
+package be.faros.sandwichbar.exception;
+
+public class SandwichbarErrorResponse {
+
+    private int errorCode;
+    private String message;
+
+
+    public SandwichbarErrorResponse(int errorCode, String message) {
+        this.errorCode = errorCode;
+        this.message = message;
+    }
+
+    public int getErrorCode() {
+        return errorCode;
+    }
+
+    public void setErrorCode(int errorCode) {
+        this.errorCode = errorCode;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
